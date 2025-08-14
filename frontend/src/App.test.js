75731788a2758710b312b404465091842167c2e8
SyedSharuk16 <126182077+SyedSharuk16@users.noreply.github.com>
2025-08-15 01:42:23 +0800

    Visualize global wastage and model info

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Smart Inventory Spoilage Predictor/i);
+  expect(heading).toBeInTheDocument();
 });
